a065a3a0ce4c243f1e839a75e1c2bff53e4a34ca
obidano <omotetedan2@gmail.com>
2022-03-24 08:58:14 +0100

    fix drf_yasg path

--- base/urls.py
+++ base/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
@@ -37,8 +37,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
   path('admin/', admin.site.urls),
-  path(r'^apis(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-  path(r'^apis/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-  path(r'^apis/redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+  re_path(r'^apis(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+  re_path(r'^apis/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+  re_path(r'^apis/redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
